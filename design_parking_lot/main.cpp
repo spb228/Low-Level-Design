a42b39bb06c5c6291aedf2630817538f0e5adf84
spb228 <spb228@cornell.edu>
2025-03-01 13:07:15 -0500

    added floor class

--- design_parking_lot/main.cpp
+++ design_parking_lot/main.cpp
@@ -17,7 +17,11 @@ public:
     Vehicle(const vehicle_type_enum v_type_in) :
         v_type(v_type_in)
     {
-        cout << "creating vehicle";
+        cout << "creating vehicle" << endl;
+    }
+
+    ~Vehicle()
+    {   
     }
 
     vehicle_type_enum get_vehicle_type()
@@ -51,14 +55,90 @@ private:
     const int semi_size = 3;  // 3 parking spots
 };
 
+class Floor
+{
+public:
+    Floor(const int floor_num_in, const int num_of_spots_in, const int num_of_rows_in) :
+        floor_num(floor_num_in),
+        num_of_spots(num_of_spots_in),
+        num_of_rows(num_of_rows_in)
+    {
+        cout << "creating floor: " << floor_num << endl; 
+        create_spots(num_of_spots); 
+    }
+
+    ~Floor()
+    {
+    }
+
+    void create_spots(const int num_of_spots)
+    {
+        spots_vec = vector<bool>(num_of_spots, false);
+    }
+
+    int get_empty_spot()
+    {
+        for (int i = 0; i < spots_vec.size(); i++)
+        {
+            if (spots_vec[i] == false)
+            {
+                return i; 
+            }
+        }
+        return -1; // no empty spots
+    }
+
+    void add_car(const int spot_num)
+    {
+        if (spot_num >= spots_vec.size() || spot_num < 0)
+        {
+            cout << "invalid spot number";
+            return;
+        }
+        spots_vec[spot_num] = true; 
+        cout << "car added to floor: " << floor_num << " , " << "spot: " << spot_num << endl; 
+    }
+
+    void remove_car(const int spot_num)
+    {
+        if (spot_num >= spots_vec.size() || spot_num < 0)
+        {
+            cout << "invalid spot number";
+            return;
+        }
+        spots_vec[spot_num] = false; 
+        cout << "car removed from floor: " << floor_num << " , " << "spot: " << spot_num << endl; 
+    }
+
+    void print_floor_spots()
+    {
+        cout << endl << "Floor: " << floor_num << endl; 
+        for (int i = 0; i < spots_vec.size(); i++)
+        {
+            cout << "Spot: " << i << " " << spots_vec[i] << endl; 
+        }
+        cout << endl;
+    }
+    
+private:
+    int floor_num;
+    int num_of_spots; 
+    int num_of_rows; 
+    vector<bool> spots_vec; // false denotes spot empty, true denotes spot taken
+};
 
 
 int main()
 {
-    cout << "test_1" << endl;
+    Vehicle sedan_1(vehicle_type_enum::SEDAN); 
+    cout << "Vehicle type: " << sedan_1.get_vehicle_type() << endl; 
+    cout << "Vehicle size: " << sedan_1.get_vehicle_size() << endl; 
     
-
-    
+    int floor_num_0 = 0; 
+    int num_spots = 10; 
+    int num_rows = 1; 
+    Floor floor_0(floor_num_0, num_spots, num_rows); 
+    floor_0.print_floor_spots(); 
 }
 
 /* 
